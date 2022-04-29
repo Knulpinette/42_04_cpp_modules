9874f2b71e1d8dfdc1dd3615554bcf1a89db45b4
Knulpinette <ophelie.surcouf@gmail.com>
2022-04-29 16:57:13 +0100

    megaphone works

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,10 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osurcouf <.@student.42lisboa.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/02 18:22:44 by osurcouf          #+#    #+#             */
+/*   Updated: 2021/06/30 17:15:30 by osurcouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <cctype>
+#include <string>
 
-int main(void) {
-	char buffer[512];
+int main(int argc, char **argv) {
+	std::string buffer;	
 
-	std::cout << "Type : ";
-	std::cin >> buffer;
-	std::cout << "You typed : " << buffer << std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; argv[i]; i++) 
+			buffer += argv[i];
+		for (int i = 0; buffer[i]; i++)
+			buffer[i] = std::toupper(buffer[i]);
+	}
+	std::cout << buffer << std::endl;
 	return (0);
 }
