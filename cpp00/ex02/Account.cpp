012cff2b84b85136887948e6a222ccf4b9b9029e
Knulpinette <ophelie.surcouf@gmail.com>
2022-06-08 18:55:45 +0100

    reorganised the house

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,6 +1,11 @@
 #include "Account.hpp"
 
-int	main(void)
-{
-	
-}
+Account::Account( int initial_deposit ){}
+Account::Account( void ){}
+Account::~Account( void ){}
+
+void	makeDeposit( int deposit ){}
+bool	makeWithdrawal( int withdrawal ){}
+int		checkAmount( void ) const{}
+void	displayStatus( void ) const{}
+static void	_displayTimestamp( void ){}
