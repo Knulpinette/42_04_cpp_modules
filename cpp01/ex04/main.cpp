c92e114d9302129cbbbd87159e781d343c08027a
Ophelie Surcouf <osurcouf@c2r7s8.42lisboa.com>
2022-07-22 15:07:46 +0100

    saving progress

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+        return (EXIT_FAILURE);
+    if (filename exists)
+        std::string filename = argv[1];
+    if (len is more than 0)
+        std::string string1 = argv[2];
+    if (len is more than 0)
+        std::string string2 = argv[3];
+
+}
