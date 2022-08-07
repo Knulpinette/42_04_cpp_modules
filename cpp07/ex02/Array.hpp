a809f4b084d4af984b21c59ae71a563f433c17be
Ophelie Surcouf <osurcouf@c2r7s8.42lisboa.com>
2022-08-07 19:46:20 +0100

    changed to Array.hpp

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -9,22 +9,68 @@ template <typename T> class Array {
         unsigned int    size;
 
     public:
-        Array();
-        Array( unsigned int n );
-        Array( const Array& valueToCopy );
-        Array<T>& operator = ( const Array<T>& valueToCopy );
-        ~Array();
+        Array() 
+            : arr(new T[0]),
+                size(0) 
+                {}
+
+        Array( unsigned int n )
+            : arr(new T[n]),
+                size(n) 
+                {}
+
+        Array( const Array& valueToCopy ) {
+            size = valueToCopy.size;
+            arr = new T(valueToCopy.size);
+            *arr = *valueToCopy.arr;
+            for (unsigned int i = 0; i < size; i++)
+                arr[i] = valueToCopy.arr[i];
+        }
+
+        Array<T>& operator = ( const Array<T>& valueToCopy ) {
+             delete[] arr;
+            size = valueToCopy.size;
+            arr = new Array(valueToCopy);
+        }
+
+        ~Array() {
+            delete[] arr;
+        }
+
 
         // Returns size of array
-        unsigned int    getSize();
+        unsigned int    getSize() {
+            return size;
+        }
 
         // Overloading accessors operators
-        T& operator[](unsigned int index);
-        const T& operator[](unsigned int index) const;
+        /*  
+            The Subscript or Array Index Operator is denoted by ‘[]’. 
+            This operator is generally used with arrays to retrieve and manipulate 
+            the array elements.
+
+                https://www.geeksforgeeks.org/overloading-subscript-or-array-index-operator-in-c/
+
+            
+            */
+        T& operator[](unsigned int index) {
+            if (index >= size)
+                throw Array<T>::OutOfBoundsException();
+            else
+                return (arr[index]);
+        }
+        const T& operator[](unsigned int index) const {
+            if (index >= size)
+                throw Array<T>::OutOfBoundsException();
+            else
+                return (arr[index]);
+        }
 
         // Throws exception when naviguating the array out of bounds.
         class OutOfBoundsException: public std::exception {
-            const char * what () const throw ();
+            const char * what () const throw () {
+                return "Index is out of bounds."; 
+            }
         };
 };
 
