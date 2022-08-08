7530ba528d1b9bc142740d83dbbe1359cee04897
Knulpinette <ophelie.surcouf@gmail.com>
2022-08-08 01:21:52 +0100

    ex00 done

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -2,5 +2,26 @@
 # define EASYFIND_HPP
 
 #include <iostream>
+#include <exception>
+/* STL library: https://www.geeksforgeeks.org/algorithms-library-c-stl/ */
+#include <vector> /* https://www.geeksforgeeks.org/vector-in-cpp-stl/ */
+#include <algorithm> /* https://www.geeksforgeeks.org/c-magicians-stl-algorithms/ */
+# include <deque>
+# include <list>
+
+template <typename T> 
+typename T::const_iterator easyfind(const T &container, int searchInt) {
+	
+	return find(container.begin(), container.end(), searchInt);
+};
+
+/* About << and iterators:
+	https://stackoverflow.com/questions/66434359/error-no-match-for-operator-operand-types-are-stdostream-aka-stdba */
+
+// For testing purposes
+template <typename T> void	printVector(const T i)
+{
+	std::cout << i << " "; 
+}
 
 #endif
