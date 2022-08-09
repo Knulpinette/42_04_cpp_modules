89ceb7279c3934b4d65388bf08201bc7069a73d2
Ophelie Surcouf <osurcouf@c2r7s8.42lisboa.com>
2022-08-09 10:50:19 +0100

    cpp DONE

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,10 +2,52 @@
 # define MUTANTSTACK_HPP
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <deque>
-#include <list>
-#include <exception>
+#include <stack>
+/*
+    Here we only need the stack header because stack is itself a template
+    using vectors and deque.
+        template <class T, class Container = deque<T> > class stack;
+    The stack has a .begin() and .end() function but they are only accessible
+    from the deque container they hold. 
+    With the MutatedStack we're creating a more direct access to (exposing) it.
+
+    (Access Syntax)
+    Type of the elements (T).
+        Aliased as member type stack::value_type.
+    Container (deque<T>)
+        Type of the internal underlying container object where the elements are stored.
+        Its value_type shall be T.
+        Aliased as member type stack::container_type. 
+        /!\ IMPORTANT
+        To access its member object (underlying container variable), use .c
+            example: mstack.c;
+            https://docs.w3cub.com/cpp/container/stack 
+    
+    Spoiler alert: if you can use c++11, there is an easy solution for all this:
+        using the 'auto' keyword.
+    */
+
+template <typename T> class MutantStack: public std::stack<T> {
+    public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        MutantStack() {}
+        MutantStack( const MutantStack& valueToCopy ) {
+            *this = valueToCopy;
+        }
+        MutantStack &operator = ( const MutantStack &valueToCopy ) {
+            this->c = valueToCopy.c;
+            return (*this);
+        }
+        ~MutantStack() {}
+
+        iterator    begin() {
+            return this->c.begin();
+        }
+
+        iterator    end() {
+            return this->c.end();
+        }
+};
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,27 +1,97 @@
-int main()
-{
-	//WRITE MY OWN TESTING ALSO!!
+#include "MutantStack.hpp"
+#include <iostream>
+#include <sstream>
+
+static void test_subject(void) {
+	std::cout << "*** Test Subject ***" << std::endl;
 
 	MutantStack<int> mstack;
+
+	std::cout << "Adding 5" << std::endl;
 	mstack.push(5);
+	std::cout << "Adding 17" << std::endl;
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+
+	std::cout << "Top: " << mstack.top() << std::endl;
+
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+
+	std::cout << "Size after pop: " << mstack.size() << std::endl;
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
+
+	++it; 	// just testing that it is possible to increment
+ 	--it;	// and decrement. Shouldn't do anything.
+	while (it != ite) {
 		std::cout << *it << std::endl;
 		++it;
 	}
+
 	std::stack<int> s(mstack);
-	return 0;
+
+	std::cout << std::endl;
+}
+
+static void test_10_numbers(void) {
+	std::cout << "*** Test 10 Numbers ***" << std::endl;
+
+	MutantStack<int> mstack;
+
+	for (int i = 0; i < 10; i++) {
+		mstack.push(i);
+	}
+
+	std::cout << "Top: " << mstack.top() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	for (MutantStack<int>::iterator it = mstack.begin(); 
+									it != mstack.end(); 
+									it++) {
+		std::cout << *it << std::endl;
+	}
+
+	// Testing Deep Copy
+	std::stack<int> s(mstack);
+
+	std::cout << std::endl;
+}
+
+static void test_10_string(void) {
+	std::cout << "*** Test 10 Strings ***" << std::endl;
+
+	MutantStack<std::string> mstack;
+
+	// adding all numbers as strings (doing a stream conversion)
+	// https://cplusplus.com/reference/sstream/stringstream/str/ 
+	for (int i = 0; i < 10; i++) {
+		std::ostringstream ss;
+		ss << i;
+		mstack.push(ss.str()); 
+	}
+
+	std::cout << "Top: " << mstack.top() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	for (MutantStack<std::string>::iterator it = mstack.begin(); 
+									it != mstack.end(); 
+									it++) {
+		std::cout << *it << std::endl;
+	}
+
+	// Testing Deep Copy
+	std::stack<std::string> s(mstack);
+
+	std::cout << std::endl;
+}
+
+int main(void) {
+	test_subject();
+	test_10_numbers();
+	test_10_string();
 }
